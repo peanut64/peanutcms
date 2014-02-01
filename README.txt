b3abc786a98505fe0a3de6f4fb1c71cbaa2bfe22
peanut64 <64peanuts@gmail.com>
2014-02-01 14:05:48 +0530

    created the project

---
+++ README.txt
@@ -0,0 +1,7 @@
+PEANUTCMS
+
+- A simple set of scripts to create a VERY simple website.
+
+- Copyright peanu64 <64peanuts@gmail.com>
+
+- Licensed under the GNU GPL (any version you like)
---
+++ web.py
@@ -0,0 +1,8 @@
+# This file is a part of PEANUTCMS
+#
+# (C) Copyright peanut64 <64peanuts@gmail.com>
+#
+# Licensed under the GNU GPL (any version you choose)
+#
+#######################################################
+
