1729f52dda8e6218757701fc3beb301dd2b6970b
peanut64 <64peanuts@gmail.com>
2014-04-24 20:50:51 +0530

    will use bottle

--- components/db.py
+++
@@ -1,8 +0,0 @@
-# This file is a part of PEANUTCMS
-#
-# (C) Copyright peanut64 <64peanuts@gmail.com>
-#
-# Licensed under the GNU GPL (any version you choose)
-#
-#######################################################
-
